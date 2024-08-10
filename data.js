6e905701489efc55a9df9989642fee15bc51c7e3
t-powers <127955989+t-powers@users.noreply.github.com>
2024-08-09 20:43:56 -0400

    Adding data to file

---
+++ data.js
@@ -0,0 +1,112 @@
+vehicleData = [
+  {
+    emotionTags: ["laid-back"],
+    image: "bentley.jpg",
+    alt: "Black shiny Bentley convertible",
+  },
+  {
+    emotionTags: ["laid-back"],
+    image: "rolls royce dawn.jpg",
+    alt: "Black shiny Rolls Royce convertible",
+  },
+  {
+    emotionTags: ["laid-back"],
+    image: "mercedes maybach.jpg",
+    alt: "Black and maroon shiny Mercedes Maybach",
+  },
+  {
+    emotionTags: ["energetic"],
+    image: "ferrari 1.jpg",
+    alt: "Bright red Ferrari sports car",
+  },
+  {
+    emotionTags: ["energetic"],
+    image: "lamborghini 1.jpg",
+    alt: "Orange Lamborghini with opened doors",
+  },
+  {
+    emotionTags: ["energetic"],
+    image: "ferrari f40.jpg",
+    alt: "Pretty red Ferrari F40 with aircraft in background",
+  },
+];
+
+/*1. **Adventurous**
+   - Land Rover Defender
+   - Mercedes-Benz G-Class (G-Wagon)
+   - Lamborghini Urus
+
+
+4. **Practical**
+   - Tesla Model X Plaid
+   - Porsche Taycan Turbo S
+   - Range Rover Autobiography
+
+5. **Elegant**
+   - Rolls-Royce Phantom
+   - Bentley Mulsanne
+   - Aston Martin DB11
+
+6. **Nostalgic**
+   - Classic Ferrari 250 GTO
+   - Jaguar E-Type
+   - Porsche 356 Speedster
+
+7. **Playful**
+   - Porsche 911 Turbo S
+   - Aston Martin Vantage
+   - Audi TT RS
+
+8. **Aggressive**
+   - McLaren 720S
+   - Dodge Challenger SRT Hellcat Redeye
+   - Lamborghini Huracán Performante
+
+9. **Confident**
+   - Mercedes-Benz G-Class (G-Wagon)
+   - Ferrari SF90 Stradale
+   - BMW M8 Competition
+
+10. **Mysterious**
+    - Aston Martin DBS Superleggera
+    - Lamborghini Aventador S (in black)
+    - Maserati Quattroporte
+
+11. **Cheerful**
+    - Ferrari Portofino
+    - Lamborghini Huracán Evo Spyder
+    - McLaren 570S Spider
+
+
+
+14. **Restless**
+    - Ducati Panigale V4
+    - Ferrari F12 Berlinetta
+    - Audi R8 V10 Plus
+
+15. **Sophisticated**
+    - Rolls-Royce Ghost
+    - Bentley Flying Spur
+    - Aston Martin Rapide
+
+16. **Free-spirited**
+    - Lamborghini Urus
+    - Maserati Levante Trofeo
+    - Range Rover SVAutobiography
+
+
+18. **Eclectic**
+    - Pagani Huayra
+    - Bugatti Veyron
+    - Koenigsegg Regera
+
+19. **Rebellious**
+    - Bugatti Chiron
+    - Lamborghini Aventador SVJ
+    - Hennessey Venom GT
+
+20. **Luxury-seeking**
+    - Koenigsegg Jesko
+    - Bugatti Divo
+    - Rolls-Royce Sweptail
+*/
