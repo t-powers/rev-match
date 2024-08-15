bf106b46c10ef539e01998cce248cfe3c666d874
t-powers <127955989+t-powers@users.noreply.github.com>
2024-08-14 21:10:28 -0400

    Log out emotion tags

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 import { vehicleData } from "./data.js";
+
+function getEmotionArray(cars) {
+  for (let car of cars) {
+    for (let tag of car.emotionTags) {
+      console.log(tag);
+    }
+  }
+}
+
