3c5980ccbe60f381517b98e1b0551e54cce7a494
t-powers <127955989+t-powers@users.noreply.github.com>
2024-08-15 20:11:20 -0400

    Create new array

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 import { vehicleData } from "./data.js";
 
+//push unique emotions to new array, return that array
 function getEmotionArray(cars) {
+  const tagArray = [];
   for (let car of cars) {
     for (let tag of car.emotionTags) {
       console.log(tag);
     }
   }
+  return tagArray;
 }
+
+/*
+
+
+
+
+*/
